2477705ab14191e621f44b32dd64e68a3b4eff37
Anton Konovalov <extempl@gmail.com>
2021-07-15 15:06:10 +0300

    initial

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+const custom = require('../config/webpack.config');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
